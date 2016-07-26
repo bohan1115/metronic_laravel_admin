3f401f407cb724e2bc88aec241e2080b03d1cc84
bohan1115 <bohan1115@126.com>
2016-07-26 23:22:52 +0800

    Add files via upload

---
+++ resources/views/widgets/pcas.blade.php
@@ -0,0 +1,15 @@
+<div class="form-group">
+    <label class="control-label col-md-3">{{$options['label']}}</label>
+    <div class="col-md-2">
+        <select class="form-control" name="province">
+        </select>
+    </div>
+    <div class="col-md-2">
+        <select class="form-control" name="city">
+        </select>
+    </div>
+    <div class="col-md-2">
+        <select class="form-control" name="county">
+        </select>
+    </div>
+</div>
---
+++ resources/views/widgets/tagesinput.blade.php
@@ -0,0 +1,6 @@
+<div class="form-group">
+	<label class="control-label col-md-3">{{$options['label']}}</label>
+	<div class="col-md-6">
+		<input id="{{$options['real_name']}}" type="text" name="{{$options['real_name']}}" value="{{$options['data'] or $options['value']}}" class="form-control tags medium" />
+	</div>
+</div>
